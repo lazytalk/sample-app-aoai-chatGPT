d4eeeba2f9dadea618b369f7e433aa3b70e0c1cb
lazytalk <lazytalk@gmail.com>
2025-03-10 21:04:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -250,7 +250,7 @@ def prepare_model_args(request_body, request_headers):
         "top_p": app_settings.azure_openai.top_p,
         "stop": app_settings.azure_openai.stop_sequence,
         "stream": app_settings.azure_openai.stream,
-        "engine": app_settings.azure_openai.model,
+        "model": app_settings.azure_openai.model,
         "user": user_json
     }
 
